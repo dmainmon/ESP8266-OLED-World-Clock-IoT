06edf00e4999b1d7f2d5c62fc9c84730ebf83fcd
dmainmon <damon@cot.net>
2017-02-05 09:33:20 -0800

    Update I2C_scanner.ino

--- I2C_scanner.ino
+++ I2C_scanner.ino
@@ -22,7 +22,7 @@ void setup()
 {
   Wire.begin();
 
-  Serial.begin(11500);
+  Serial.begin(115200);
   while (!Serial);             // Leonardo: wait for serial monitor
   Serial.println("\nI2C Scanner");
 }
