37329b8feeede454dd31c35a868ba259e835605b
dmainmon <damon@cot.net>
2017-02-05 09:31:47 -0800

    I2C_Scanner.ino
    Used to determine the I2C address on the OLED display.

---
+++ I2C_scanner.ino
@@ -0,0 +1,71 @@
+// --------------------------------------
+// i2c_scanner
+//
+// To use with an ESP8266-12E
+//    GND -> GND
+//    VCC -> 3V3
+//    SCL -> D1
+//    SDA -> D2
+//
+//
+// To use with Arduino UNO
+// SDA is A4 and SCL is A5
+//
+// This sketch tests the standard 7-bit addresses
+// Devices with higher bit address might not be seen properly.
+//
+
+#include <Wire.h>
+
+
+void setup()
+{
+  Wire.begin();
+
+  Serial.begin(11500);
+  while (!Serial);             // Leonardo: wait for serial monitor
+  Serial.println("\nI2C Scanner");
+}
+
+
+void loop()
+{
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
+}
